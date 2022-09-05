a786d4ae2137ad8f00f7678255f46859c877e5d7
kdnakt <a.kid.1985@gmail.com>
2022-09-05 09:54:01 +0900

    add ClientFinished constructor

--- lib/src/main/java/com/kdnakt/tls/ClientFinished.java
+++ lib/src/main/java/com/kdnakt/tls/ClientFinished.java
@@ -5,6 +5,10 @@ import java.io.OutputStream;
 
 public class ClientFinished {
 
+    public ClientFinished(ClientHello clientHello, ServerHello sh, Certificate certificate, ServerKeyExchange ske,
+            ServerHelloDone done, ClientKeyExchange cke, ClientChangeCipherSpec cccs) {
+    }
+
     public void writeTo(OutputStream out) throws IOException {
         byte[] message = null;
         out.write(message);
