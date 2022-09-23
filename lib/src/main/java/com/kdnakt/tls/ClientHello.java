d0767af83da93f215222f689006a33ff7ae4c3f2
kdnakt <a.kid.1985@gmail.com>
2022-09-23 15:10:16 +0900

    add cipher suites names

--- lib/src/main/java/com/kdnakt/tls/ClientHello.java
+++ lib/src/main/java/com/kdnakt/tls/ClientHello.java
@@ -40,8 +40,22 @@ public class ClientHello implements HandshakeMessage {
             0x00,
             // Cipher Suites
             0x00, 0x20, // 32 bytes follows
-            0xcc, 0xa8, 0xcc, 0xa9, 0xc0, 0x2f, 0xc0, 0x30, 0xc0, 0x2b, 0xc0, 0x2c, 0xc0, 0x13, 0xc0, 0x09,
-            0xc0, 0x14, 0xc0, 0x0a, 0x00, 0x9c, 0x00, 0x9d, 0x00, 0x2f, 0x00, 0x35, 0xc0, 0x12, 0x00, 0x0a,
+            0xcc, 0xa8, // TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256
+            0xcc, 0xa9, // TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256
+            0xc0, 0x2f, // TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256
+            0xc0, 0x30, // TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384
+            0xc0, 0x2b, // TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256
+            0xc0, 0x2c, // TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384
+            0xc0, 0x13, // TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA
+            0xc0, 0x09, // TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA
+            0xc0, 0x14, // TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA
+            0xc0, 0x0a, // TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA
+            0x00, 0x9c, // TLS_RSA_WITH_AES_128_GCM_SHA256
+            0x00, 0x9d, // TLS_RSA_WITH_AES_256_GCM_SHA384
+            0x00, 0x2f, // TLS_RSA_WITH_AES_128_CBC_SHA
+            0x00, 0x35, // TLS_RSA_WITH_AES_256_CBC_SHA
+            0xc0, 0x12, // TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA
+            0x00, 0x0a, // TLS_RSA_WITH_3DES_EDE_CBC_SHA
             // Compression method
             0x01, 0x00,
             // Extensions length
